728ccad16c5747b18f4b762759d09b388e5ceb4e
Raul Bernal <bernal.raul@gmail.com>
2020-11-11 16:24:57 +0100

    Update and rename bootstrap_and_full_backup.sh to auto_bootstrap.sh

--- auto_bootstrap.sh
+++ auto_bootstrap.sh
@@ -8,11 +8,7 @@ cat blk000*.dat > /media/backup/bootstrap.dat
 touch /media/backup/bootstrap-block-$LAST
 7z a -tzip /media/backup/bootstrap-$LAST.zip /media/backup/bootstrap.dat  /media/backup/bootstrap-block-$LAST
 rm /media/backup/bootstrap.dat
-rm  /media/backup/bootstrap-block-$LAST
-echo .
-echo Making new FULL BACKUP
-rm /media/backup/full_backup*
-7z a -xr@/home/bitcanna/backup_bitcanna/exclude.txt  /media/backup/full_backup_bcna_nowallets-$LAST.7z /home/bitcanna/.bitcanna/
+rm /media/backup/bootstrap-block-$LAST
 echo .
 echo End of backups... starting daemon again
 bitcannad -daemon
